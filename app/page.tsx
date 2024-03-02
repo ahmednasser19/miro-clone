a5aea38aa7eed6d2635e0428601efad032fe7976
ahmed nasser <ahmednas197@gmail.com>
2024-03-02 19:29:21 +0200

    add auth using clerk and connect with convex backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <div className=" text-red-500">
       <Button> Click</Button>
       Hello Miro App
+      <div>
+        <UserButton />
+      </div>
     </div>
   );
 }
